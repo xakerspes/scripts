bad36d9f0709e74c5309c66fadfa815fb949f6eb
xakerspes <38161328+xakerspes@users.noreply.github.com>
2022-07-21 21:50:19 +0500

    Create install.py

---
+++ install.py
@@ -0,0 +1,11 @@
+Postgresql: 
+    video:   https://www.youtube.com/watch?v=wWJAE3gZIvM
+    apt install postgresql postgresql-contrib
+    systemctl start postgresql.service
+    postgresql-14.4-1
+    python-django:
+      apt install libpq-dev
+      python3 -m pip3 install psycopg2
+      
+    
+      
